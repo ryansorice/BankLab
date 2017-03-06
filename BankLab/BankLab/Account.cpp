d2280fc4056f1bdce575050c8fda462493e29a40
ncc <ncc>
2017-03-06 10:04:14 -0800

    Started working  on Account Class

--- BankLab/BankLab/Account.cpp
+++ BankLab/BankLab/Account.cpp
@@ -1,12 +1,25 @@
 #include "Account.h"
 
 
-
-Account::Account()
+Account::Account(Customer customer, int accountNumber) : _accountOwner(customer), _accountNumber(accountNumber)
 {
 }
 
-
 Account::~Account()
 {
+}
+
+void Account::Deposit(int numberOfPennies)
+{
+	_balanceInPennies += numberOfPennies;
+}
+
+void Account::Withdraw(int numberOfPennies)
+{
+	_balanceInPennies -= numberOfPennies;
+}
+
+int Account::getBalance()
+{
+	return _balanceInPennies;
 }
--- BankLab/BankLab/Account.h
+++ BankLab/BankLab/Account.h
@@ -1,9 +1,18 @@
 #pragma once
+#include "Customer.h"
+
 class Account
 {
 private:
+	int _accountNumber;
+	int _balanceInPennies = 0;
+	Customer _accountOwner;
 public:
-	Account();
+	Account(Customer customer, int accountNumber);
 	~Account();
+
+	void Deposit(int numberOfPennies);
+	void Withdraw(int numberOfPennies);
+	int getBalance();
 };
 
